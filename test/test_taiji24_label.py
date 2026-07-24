585a5da7b8226a6b89cc1dd747f6b43b7017b53a
TZXL3029 <15197960+luctrix@user.noreply.gitee.com>
2026-07-24 18:06:05 +0800

    improve taiji24 testB labeling

--- test/test_taiji24_label.py
+++ test/test_taiji24_label.py
@@ -86,6 +86,93 @@ class TestTaiji24Label(unittest.TestCase):
             self.assertEqual(by_index[101]["big_label"], "irrelevant")
             self.assertEqual(by_index[101]["action_label"], "closing")
 
+    def test_auto_label_directory_handles_noisy_testb_boundaries(self):
+        names = [
+            "testB_cut_1_24世太极.mp4",
+            "testB_cut_2_分角开步.mp4",
+            "testB_cut_6_呼气下安.mp4",
+            "testB_cut_7_左右野马分.mp4",
+            "testB_cut_14_白喝量赤.mp4",
+            "testB_cut_18_左右捞稀澳.mp4",
+            "testB_cut_25_手灰琵琶.mp4",
+            "testB_cut_26_左右道卷工.mp4",
+            "testB_cut_29_灼烂却尾.mp4",
+            "testB_cut_36_公布庞守.mp4",
+            "testB_cut_43_元首三.mp4",
+            "testB_cut_44_勾手转体.mp4",
+            "testB_cut_48_双风罐.mp4",
+            "testB_cut_50_转身左灯角.mp4",
+            "testB_cut_53_是我下世独.mp4",
+            "testB_cut_56_又下时都没.mp4",
+            "testB_cut_58_左右传说.mp4",
+            "testB_cut_63_虚不叉仗.mp4",
+            "testB_cut_64_转身斑斓.mp4",
+            "testB_cut_68_如风似壁.mp4",
+            "testB_cut_72_交叉搭手.mp4",
+            "testB_cut_75_并无怀缘.mp4",
+        ]
+
+        with tempfile.TemporaryDirectory() as temp_dir:
+            for name in names:
+                open(os.path.join(temp_dir, name), "wb").close()
+
+            rows = auto_label_directory(temp_dir)
+            by_index = {int(row["segment_index"]): row for row in rows}
+
+            self.assertEqual(by_index[1]["big_label"], "irrelevant")
+            self.assertEqual(by_index[2]["action_label"], "01_起势")
+            self.assertEqual(by_index[7]["action_label"], "02_左右野马分鬃")
+            self.assertEqual(by_index[14]["action_label"], "03_白鹤亮翅")
+            self.assertEqual(by_index[18]["action_label"], "04_左右搂膝拗步")
+            self.assertEqual(by_index[25]["action_label"], "05_手挥琵琶")
+            self.assertEqual(by_index[26]["action_label"], "06_左右倒卷肱")
+            self.assertEqual(by_index[29]["action_label"], "07_左揽雀尾")
+            self.assertEqual(by_index[36]["action_label"], "08_右揽雀尾")
+            self.assertEqual(by_index[43]["action_label"], "10_云手")
+            self.assertEqual(by_index[44]["action_label"], "11_单鞭")
+            self.assertEqual(by_index[48]["action_label"], "14_双峰贯耳")
+            self.assertEqual(by_index[50]["action_label"], "15_转身左蹬脚")
+            self.assertEqual(by_index[53]["action_label"], "16_左下势独立")
+            self.assertEqual(by_index[56]["action_label"], "17_右下势独立")
+            self.assertEqual(by_index[58]["action_label"], "18_左右穿梭")
+            self.assertEqual(by_index[63]["action_label"], "19_海底针")
+            self.assertEqual(by_index[64]["action_label"], "21_转身搬拦捶")
+            self.assertEqual(by_index[68]["action_label"], "22_如封似闭")
+            self.assertEqual(by_index[72]["action_label"], "23_十字手")
+            self.assertEqual(by_index[75]["action_label"], "24_收势")
+
+    def test_auto_label_directory_handles_noisy_ordinal_suffixes(self):
+        names = [
+            "testB_cut_81_第十四世.mp4",
+            "testB_cut_83_第15式.mp4",
+            "testB_cut_89_一十六十.mp4",
+            "testB_cut_93_第17世.mp4",
+            "testB_cut_99_第十八事.mp4",
+            "testB_cut_106_第19时.mp4",
+            "testB_cut_111_第21世.mp4",
+            "testB_cut_116_第22史.mp4",
+            "testB_cut_120_第23世.mp4",
+            "testB_cut_123_第24集.mp4",
+        ]
+
+        with tempfile.TemporaryDirectory() as temp_dir:
+            for name in names:
+                open(os.path.join(temp_dir, name), "wb").close()
+
+            rows = auto_label_directory(temp_dir)
+            by_index = {int(row["segment_index"]): row for row in rows}
+
+            self.assertEqual(by_index[81]["action_label"], "14_双峰贯耳")
+            self.assertEqual(by_index[83]["action_label"], "15_转身左蹬脚")
+            self.assertEqual(by_index[89]["action_label"], "16_左下势独立")
+            self.assertEqual(by_index[93]["action_label"], "17_右下势独立")
+            self.assertEqual(by_index[99]["action_label"], "18_左右穿梭")
+            self.assertEqual(by_index[106]["action_label"], "19_海底针")
+            self.assertEqual(by_index[111]["action_label"], "21_转身搬拦捶")
+            self.assertEqual(by_index[116]["action_label"], "22_如封似闭")
+            self.assertEqual(by_index[120]["action_label"], "23_十字手")
+            self.assertEqual(by_index[123]["action_label"], "24_收势")
+
     def test_pinyin_fallback_includes_taiji_terms(self):
         self.assertEqual(text_to_pinyin("倒卷肱"), "daojuangong")
         self.assertEqual(text_to_pinyin("搬拦捶"), "banlanchui")
